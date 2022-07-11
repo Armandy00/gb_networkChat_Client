8754d5dc502e0ce33eac64953516f413984cf2d1
Armandy <kamelija_2000@mail.ru>
2022-07-11 12:55:43 +0300

    Lesson5_v2

--- src/main/java/com/example/gb_networkchat_client/HelloController.java
+++ src/main/java/com/example/gb_networkchat_client/HelloController.java
@@ -4,47 +4,66 @@ import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.ResourceBundle;
+
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
 public class HelloController {
 
-        private SimpleDateFormat simpleDateFormat;
+    private SimpleDateFormat simpleDateFormat;
 
-        @FXML
-        private ResourceBundle resources;
+    @FXML
+    private ResourceBundle resources;
 
-        @FXML
-        private URL location;
+    @FXML
+    private URL location;
 
-        @FXML
-        private Label labelName;
+    @FXML
+    private Label labelName;
 
-        @FXML
-        private TextField messageText;
+    @FXML
+    private TextField messageText;
 
-        @FXML
-        private ListView<String> messagesArea;
+    @FXML
+    private ListView<String> messagesArea;
 
-        @FXML
-        void sendMessageInChat(ActionEvent event) {
-                String message  = messageText.getText().trim();
+    @FXML
+    private ListView<String> userList;
 
-                if (message.length() != 0) {
-                        message = "Я:  " + message + "  " + simpleDateFormat.format(new Date());
-                        messagesArea.getItems().add(message);
-                        messageText.clear();
+    @FXML
+    private Button sendButton;
 
-                }
+    @FXML
+    void sendMessageInChat() {
+        String message = messageText.getText().trim();
 
+        if (message.isEmpty()) {
+            return;
         }
 
-        @FXML
-        void initialize() {
-                simpleDateFormat = new SimpleDateFormat("HH:mm:ss");
-        }
+        appendMessage(message);
+
+
+    }
+
+    private void appendMessage(String message) {
+        message = "Я:  " + message + "  " + simpleDateFormat.format(new Date());
+        messagesArea.getItems().add(message);
+        messageText.clear();
+    }
+
+    @FXML
+    void initialize() {
+        simpleDateFormat = new SimpleDateFormat("HH:mm:ss");
+        userList.setItems(FXCollections.observableArrayList("Екатерина", "Александр", "Юлия", "Дария"));
+        sendButton.setOnAction(event -> sendMessageInChat());
+        messageText.setOnAction(event -> sendMessageInChat());
+
+    }
 
 }
